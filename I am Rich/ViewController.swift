92b0a4aa024a18697b87c47b49255aa01a63d29d
Moshiur <moshiur19121997@gmail.com>
2024-04-09 23:14:17 +0600

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Moshiur Rahman on 7/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
